77cba68680f9ebea0dc8a13dbc39a3adb8ff362d
alexander <a.murashkin@ticketland.ru>
2017-12-11 23:54:53 +0300

    velikolepen

--- mlsk.py
+++ mlsk.py
@@ -1,9 +1,43 @@
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 from sklearn import datasets
+from sklearn.model_selection import learning_curve
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes, scoring='accuracy')
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    # train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    # plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+    #                  train_scores_mean + train_scores_std, alpha=0.1,
+    #                  color="r")
+    # plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+    #                  test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
 
 if __name__ == "__main__":
     models = [
@@ -55,6 +89,10 @@ if __name__ == "__main__":
         model = MLPClassifier(**i['params'])
 
         model.fit(X_train, y_train)
+
+        plot_learning_curve(model, "Learning", X_train, y_train)
+        plt.show()
+
         pred = model.predict(X_test)
         print(pred)
         print(y_test)
